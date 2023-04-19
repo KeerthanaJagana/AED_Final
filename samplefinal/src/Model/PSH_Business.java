da8ae31f680a60604891a79973776cc95c02ea53
Keerthana Jagana <jagana.k@northeastern.edu>
2023-04-19 01:55:06 -0400

    Login UI

--- samplefinal/src/Model/PSH_Business.java
+++ samplefinal/src/Model/PSH_Business.java
@@ -13,14 +13,65 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 public class PSH_Business {
+    private List<PSH_location> listOfLocation;
+    private PSH_CustomerDirectory customerDirectory;
     private Map<String, String> mapType;
     private Map<String, String> userNamePasswordMap;
     public PSH_Business(String uname,String pwd){
+        listOfLocation = new ArrayList<>();
+        listOfLocation.add(new PSH_location("Boston"));
         mapType = new HashMap<>();
         userNamePasswordMap = new HashMap<>();
         userNamePasswordMap.put(uname, pwd);
         addUser(uname, pwd, "admin");
     }
+
+    public List<PSH_location> getListOfLocation() {
+        return listOfLocation;
+    }
+
+    public void setListOfLocation(List<PSH_location> listOfLocation) {
+        this.listOfLocation = listOfLocation;
+    }
+
+    public PSH_CustomerDirectory getCustomerDirectory() {
+        return customerDirectory;
+    }
+
+    public void setCustomerDirectory(PSH_CustomerDirectory customerDirectory) {
+        this.customerDirectory = customerDirectory;
+    }
+
+    public Map<String, String> getMapType() {
+        return mapType;
+    }
+
+    public void setMapType(Map<String, String> mapType) {
+        this.mapType = mapType;
+    }
+
+    public Map<String, String> getUserNamePasswordMap() {
+        return userNamePasswordMap;
+    }
+
+    public void setUserNamePasswordMap(Map<String, String> userNamePasswordMap) {
+        this.userNamePasswordMap = userNamePasswordMap;
+    }
+    
+    public void addLocation(String name) {
+        PSH_location loc = new PSH_location(name);
+        listOfLocation.add(loc);
+    }
+
+    public PSH_location findServiceLocation(String LocationName) {
+        for (PSH_location loc : listOfLocation) {
+            if (listOfLocation != null && loc.getName().equals(LocationName)) {
+                return loc;
+            }
+        }
+        return null;
+    }
+    
     public void addUser(String username, String password, String type) {
         mapType.put(username, type);
         userNamePasswordMap.put(username, password);
@@ -36,5 +87,28 @@ public class PSH_Business {
     }
     public static PSH_Business createSystemAdmin() {
         return new PSH_Business("admin", "admin");
+    }
+    public boolean userExistsInSystem(String username) {
+        return mapType.containsKey(username);
+    }
+    
+    public void deleteCustomer(PSH_Customer customer) {
+        customerDirectory.getListOfCustomer().remove(customer);
+        mapType.remove(customer.getUserName());
+    }
+
+    public void updateUser(String username, String password) {
+        if (userNamePasswordMap.containsKey(username)) {
+            userNamePasswordMap.put(username, password);
+        }
+    }
+
+    public PSH_Customer findCustomer(String user) {
+        for (PSH_Customer custom : customerDirectory.getListOfCustomer()) {
+            if (custom.getUserName().equals(user)) {
+                return custom;
+            }
+        }
+        return null;
     }
 }
